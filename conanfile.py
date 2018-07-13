a296511a51aea279cc4ca4d94f904156f27762f9
Darlan Cavalcante Moreira <darcamo@gmail.com>
2018-07-13 18:51:42 -0300

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,28 @@
+from conans import ConanFile, CMake, tools
+import shutil
+import os
+
+
+class NlohmannjsonConan(ConanFile):
+    name = "nlohmann-json"
+    version = "3.1.2"
+    license = "MIT"
+    url = "https://github.com/darcamo/conan-nlohmann_json"
+    description = "JSON for Modern C++"
+    no_copy_source = True
+    homepage = "https://github.com/nlohmann/json#examples"
+    # No settings/options are necessary, this is header only
+
+    def source(self):
+        tools.get("https://github.com/nlohmann/json/archive/v{0}.zip".format(self.version))
+        shutil.move("json-{0}".format(self.version), "sources")
+
+    def build(self):
+        os.mkdir("build")
+        cmake = CMake(self)
+        cmake.configure(source_folder="sources", build_folder="build")
+        cmake.build()
+        cmake.install()
+
+    # def package(self):
+    #     self.copy("*.hpp")
